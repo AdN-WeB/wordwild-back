eaefce7582fdc26404006cdb7f43daa40e1e9775
adrien <adrien.mervelay84@gmail.com>
2022-03-21 11:51:45 +0100

    fist commit

---
+++ src/main/java/com/checkpoint/worldwild/WorldwildApplication.java
@@ -0,0 +1,13 @@
+package com.checkpoint.worldwild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldwildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldwildApplication.class, args);
+	}
+
+}
